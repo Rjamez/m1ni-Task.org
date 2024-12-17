0916424a89e7949bdfa201a66f2b170d21a87fdd
Rjamez <robin.adhola.student@moringaschool.com>
2024-12-17 23:07:37 +0300

    yup

--- app/config.py
+++
@@ -1,7 +0,0 @@
-import os  
-
-class Config:  
-    """Base configuration."""  
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///tasks.db')  
-    SQLALCHEMY_TRACK_MODIFICATIONS = False  
-    SECRET_KEY = os.getenv('SECRET_KEY', 'your_secret_key')  # You may use this for security purposes
--- app/lib/main.py
+++
@@ -1,9 +0,0 @@
-from flask import app
-from lib.cli import cli  
-
-if __name__ == '__main__':  
-    cli()
-
-from config import Config  
-
-app.config.from_object(Config)
